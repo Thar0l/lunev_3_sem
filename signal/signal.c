ae358a8c721fb4505f2d4f6af10671ba3d29180d
Thar0l <thar0l@thar0l-VirtualBox.(none)>
2013-10-25 21:34:39 +0400

    [signal] Program refactored to readable format

--- signal/signal.c
+++ signal/signal.c
@@ -6,43 +6,23 @@
 #include <string.h>
 #include <math.h>
 
-int connect;
-char data;
-int childpid;
-int parentpid;
+
+//Global
+
+int 	connection;
+char 	bit;
+int 	childpid;
+int 	parentpid;
+
+void get_signal_die();
+void get_signal_alarm();
+void get_signal_sync();
+void get_signal_0();
+void get_signal_1();
 
 
 
-void getChildSignalDie()
-{
-		printf("\n");
-		kill(childpid, SIGKILL);
-		exit(0);
-}
-
-void getAlarmSignal()
-{
-		exit(0);
-}
-
-void getParentSignal()
-{
-	connect=1;
-}
-
-void getChildSignal0()
-{
-	connect=1;
-	data=0;
-}
-
-void getChildSignal1()
-{
-	connect=1;
-	data=1;
-}
-
-int main(int argc, char** argv)
+int main (int argc, char** argv)
 {
 parentpid = getpid();
 childpid = fork();
@@ -50,176 +30,215 @@ childpid = fork();
 
 if (childpid == -1) 
 {	
-		perror("fork");
-		exit(EXIT_FAILURE);
+		perror ("fork");
+		exit (EXIT_FAILURE);
 }
 	
- /* Code executed by child */	
-if (childpid == 0) 
-{           
-		int readresult;
-		int o;
-		int n;
-		FILE *f = fopen("1.txt", "rb");
-		if (!f)
-		{
-			perror("Opening file: ");
-			return -1;
-		}
-		sigset_t set,set0;
-		
-		sigemptyset(&set);
-		sigaddset(&set,SIGPIPE);
-		sigprocmask(SIG_BLOCK,&set,NULL);
-		
-		sigemptyset(&set0);
-		sigaddset(&set0,SIGALRM);
-		sigprocmask(SIG_UNBLOCK,&set0,NULL);
-		
-		struct sigaction act,alrm;
-		
-		
-		memset(&act,0,sizeof(act));
-		act.sa_handler=getParentSignal;
-		sigaction(SIGPIPE,&act,NULL);
-		
-		memset(&alrm,0,sizeof(alrm));
-		alrm.sa_handler=getAlarmSignal;
-		sigaction(SIGALRM,&alrm,NULL);
-		
-		n=0;
-		readresult=fscanf(f,"%c",&data);
-		alarm(10);
-		while (readresult>0)
-		{
-			
-			connect=0;
-			
-			sigprocmask(SIG_UNBLOCK,&set,NULL);		
-			alarm(10);
-			while (!connect) {}
-			alarm(10);
-					
-			sigprocmask(SIG_BLOCK,&set,NULL);
-			if (n<7) 
-			{
-				int a;
-				a=(data&(1<<n))>>n;
-				if (a==0) 
-				{
-					if (kill(parentpid, SIGUSR1)<0) exit(-1);
-				} else 
-				{
-					if (kill(parentpid, SIGUSR2)<0) exit(-1);
-				}
-//				printf("Send: %d\n",a);
-				fflush(stdout);
-				n++;
-			} else
-			{
-				int a;
-				a=(data&(1<<n))>>n;
-				if (a==0) 
-				{
-					if (kill(parentpid, SIGUSR1)<0) exit(-1);
-				}	 else 
-				{
-					if (kill(parentpid, SIGUSR2)<0) exit(-1);
-				}
-//				printf("Send: %d\n",a);
-				fflush(stdout);
-				readresult=fscanf(f,"%c",&data);
-				n=0;
-				connect=1;
-			}
-			/*
-			if (data=='0') kill(parentpid, SIGUSR1); else
-			if (data=='1') kill(parentpid, SIGUSR2); else
-			{
-				exit(0);			
-			}
-			* */
-		}
 
-		exit(0);
-} else 
-	
-/* Code executed by parent */	
-{                    
-		connect=0;
-		sigset_t set0,set1,setdie;
-		
-		sigemptyset(&set0);
-		sigaddset(&set0,SIGUSR1);
-		
-		sigemptyset(&set1);
-		sigaddset(&set1,SIGUSR2);
-		
-		sigemptyset(&setdie);
-		sigaddset(&setdie,SIGCHLD);
-		
-		struct sigaction act0,act1,actdie;
-		
-		memset(&act0,0,sizeof(act0));
-		act0.sa_handler=getChildSignal0;
-		sigaction(SIGUSR1,&act0,NULL);
-		
-		memset(&act1,0,sizeof(act1));
-		act1.sa_handler=getChildSignal1;
-		sigaction(SIGUSR2,&act1,NULL);
-		
-		memset(&actdie,0,sizeof(actdie));
-		actdie.sa_handler=getChildSignalDie;
-		sigaction(SIGCHLD,&actdie,NULL);
-		
-		sigprocmask(SIG_UNBLOCK,&set0,NULL);
-		sigprocmask(SIG_UNBLOCK,&set1,NULL);
-		sigprocmask(SIG_UNBLOCK,&setdie,NULL);
-		
-		sleep(1);
-		
-		kill(childpid, SIGPIPE);
-		int n=0;
-		char outdata=0;
-		while (1)
-		{
-			sigprocmask(SIG_UNBLOCK,&set0,NULL);
-			sigprocmask(SIG_UNBLOCK,&set1,NULL);	
-			while (!connect) {};
-			connect=0;
-			sigprocmask(SIG_BLOCK,&set0,NULL);
-			sigprocmask(SIG_BLOCK,&set1,NULL);
+	if (childpid == 0) 
+	{           
+			int 	read_result;
+			int 	index;
+			FILE 	*file; 
 			
-			if (n<7)
+			connection=0;
+			
+			file = fopen ("in.txt", "rb");
+			if (!file)
 			{
-//				printf("Get: %d\n",data);
-				fflush(stdout);
-				outdata=outdata+(data<<n);
-				n++;
-			} else
-			{
-//				printf("Get_: %d\n",data);
-				fflush(stdout);
-				fflush(stdout);
-				outdata=outdata+(data<<n);
-				n=0;
-				printf("%c",outdata);
-				fflush(stdout);
-				outdata=0;
-				
-				//connect=1;
+				perror ("Opening file: ");
+				return -1;
 			}
-			/*
-			printf("%d",data);
-			fflush(stdout);
-			*/
+			sigset_t sigset_pipe, sigset_alarm;
+			
+			sigemptyset (&sigset_pipe);
+			sigemptyset (&sigset_alarm);
+			
+			sigaddset (&sigset_pipe,  SIGPIPE);
+			sigaddset (&sigset_alarm, SIGALRM);
+			
+			sigprocmask (SIG_BLOCK,   &sigset_pipe,  NULL);		
+			sigprocmask (SIG_UNBLOCK, &sigset_alarm, NULL);
+			
+			struct sigaction sigact_pipe;
+			struct sigaction sigact_alarm;
+			
+			
+			memset (&sigact_pipe,  0, sizeof(sigact_pipe));
+			memset (&sigact_alarm, 0, sizeof(sigact_alarm));
+			
+			sigact_alarm . sa_handler = get_signal_alarm;
+			sigact_pipe  . sa_handler = get_signal_sync;
+			
+			sigaction (SIGPIPE, &sigact_pipe,  NULL);
+			sigaction (SIGALRM, &sigact_alarm, NULL);
+			
+			index = 0;
+			
+			read_result = fscanf (file, "%c", &bit);
+			
+			alarm(10);
+			
+			while (read_result>0)
+			{
+				
+				connection=0;
+				
+				sigprocmask(SIG_UNBLOCK,&sigset_pipe,NULL);		
+				
+				alarm(10);
+				while (!connection) {}
+				alarm(10);
+						
+				sigprocmask(SIG_BLOCK,&sigset_pipe,NULL);
+				
+				if (index < 7) 
+				{
+					int a;
+					
+					a = (bit & (1 << index)) >> index;
+					
+					if (a == 0) 
+					{
+						if (kill (parentpid, SIGUSR1)<0) exit(-1);
+					} 
+					else 
+					{
+						if (kill (parentpid, SIGUSR2)<0) exit(-1);
+					}
+
+					index++;
+					
+				}
+				else
+				{
+					int a;
+					
+					a = (bit & (1 << index)) >> index;
+					
+					if (a == 0) 
+					{
+						if (kill (parentpid, SIGUSR1)<0) exit(-1);
+					}	 
+					else 
+					{
+						if (kill (parentpid, SIGUSR2)<0) exit(-1);
+					}
+
+					read_result = fscanf (file, "%c", &bit);
+					
+					index = 0;
+					connection = 1;
+				}
+			}
+			exit(0);
+	} 
+	else 
+	{                    
+			
+			int 	n    = 0;
+			char	data = 0;
+			
+			connection = 0;
+			
+			sigset_t sigset_usr1, sigset_usr2, sigset_child;
+			
+			sigemptyset (&sigset_usr1);
+			sigemptyset (&sigset_usr2);
+			sigemptyset (&sigset_child);
+			
+			sigaddset (&sigset_usr1,  SIGUSR1);		
+			sigaddset (&sigset_usr2,  SIGUSR2);		
+			sigaddset (&sigset_child, SIGCHLD);
+			
+			struct sigaction sigact_usr1, sigact_usr2, sigact_child;
+			
+			memset (&sigact_usr1,  0, sizeof(sigact_usr1));
+			memset (&sigact_usr2,  0, sizeof(sigact_usr2));
+			memset (&sigact_child, 0, sizeof(sigact_child));
+			
+			sigact_usr1  . sa_handler  = get_signal_0;
+			sigact_usr2  . sa_handler  = get_signal_1;
+			sigact_child . sa_handler  = get_signal_die;
+			
+			sigaction (SIGUSR1, &sigact_usr1,  NULL);		
+			sigaction (SIGUSR2, &sigact_usr2,  NULL);		
+			sigaction (SIGCHLD, &sigact_child, NULL);
+			
+			sigprocmask (SIG_UNBLOCK, &sigset_usr1,  NULL);
+			sigprocmask (SIG_UNBLOCK, &sigset_usr2,  NULL);
+			sigprocmask (SIG_UNBLOCK, &sigset_child, NULL);
+			
+			sleep(1);
+			
 			kill(childpid, SIGPIPE);
-	
-		}
-		
-wait(NULL);
-	
-}
+			
+
+			
+			while (1)
+			{
+				sigprocmask (SIG_UNBLOCK, &sigset_usr1, NULL);
+				sigprocmask (SIG_UNBLOCK, &sigset_usr2, NULL);	
+				
+				while (!connection) {};
+				
+				connection = 0;
+				
+				sigprocmask(SIG_BLOCK, &sigset_usr1, NULL);
+				sigprocmask(SIG_BLOCK, &sigset_usr2, NULL);
+				
+				data = data + (bit << n);
+				
+				if (n < 7)
+				{
+					n++;
+				} 
+				else
+				{
+					printf ("%c", data);
+					fflush (stdout);
+					
+					data = 0;
+					n = 0;
+				}
+				kill(childpid, SIGPIPE);
+			}
+	wait(NULL);
+	}
 
 return 0;
+}
+
+
+void get_signal_die()
+{
+	printf("\n");
+	kill(childpid, SIGKILL);
+	exit(0);
+}
+
+
+void get_signal_alarm()
+{
+	exit(0);
+}
+
+
+void get_signal_sync()
+{
+	connection = 1;
+}
+
+
+void get_signal_0()
+{
+	connection = 1;
+	bit = 0;
+}
+
+
+void get_signal_1()
+{
+	connection = 1;
+	bit = 1;
 }
