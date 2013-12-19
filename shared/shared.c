77a48fb9f64776b581f0977b1221d4ec646f2ead
Thar0l <gusarov.aa@gmail.com>
2013-12-19 12:30:21 +0400

    [shared] Added last working version of program

--- shared/shared.c
+++ shared/shared.c
@@ -1,214 +1,193 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <string.h>
 #include <errno.h>
-#include <sys/sysinfo.h>       
-#include <sys/ipc.h>
-#include <sys/shm.h>
-
-#include <sys/types.h>
-#include <sys/ipc.h>
 #include <sys/sem.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+
+#define BUFF_SIZE 1024
+
+#define SEM_READER 0
+#define SEM_WRITER 1
+#define SEM_EMPTY  2
+#define SEM_FULL   3
+#define SEM_SYNC   4
+
+union semun{
+	int val;
+	unsigned short *array;
+};
 
 
 
-#define WRITER 1
-#define READER 0
-#define BUFF_SIZE 1
-
-
-
-void P (int semid)
+void addtobuff(struct sembuf *buff, unsigned short semnum, short op, short flag)
 {
-	struct sembuf mybuf;
-	mybuf . sem_op 	= -1;
-	mybuf . sem_flg = SEM_UNDO;
-	mybuf . sem_num = 0;
-	semop (semid, &mybuf, 1);
+	buff->sem_num = semnum;
+	buff->sem_op  = op;
+	buff->sem_flg = flag;
 }
 
-void V (int semid)
+
+int main(int argc, char* argv[])
 {
-	struct sembuf mybuf;
-	mybuf . sem_op	 = 1;
-	mybuf . sem_flg	 = SEM_UNDO;
-	mybuf . sem_num  = 0;
-	semop (semid, &mybuf, 1);
-}
-
-int sem_get (char file[], int offset)
-{
-	int semid = semget (ftok (file, 0) + offset, 1, IPC_CREAT | 0664);
-	if (semid < 0)
-	{
-		perror ("semget: ");
-		exit (EXIT_FAILURE);
-	}
-	return semid;
-}
-
-int mem_get (char file[], int offset)
-{
-	int shmid;
-	if (shmid = shmget(ftok(file, 0) + offset, sizeof(char) * BUFF_SIZE, 0664 | IPC_CREAT | IPC_EXCL) < 0)
-    {
-		if (errno == EEXIST)
-		{ 
-			shmid = shmget (ftok (file, 0) + 3, sizeof(char) * BUFF_SIZE, 0);
-		}
-		else
-		{
-			perror ("shmget (exist): ");
-			exit (EXIT_FAILURE);
-		}
-		
-	}
-	else
-	{
-		perror ("shmget (create): ");
-		exit (EXIT_FAILURE);
-	}
-	return shmid;
-}
-
-int  main(int argc, char *argv[])
-{
-
-if (argc > 2) 
-{
-	fprintf(stderr, "Usage: %s <filename> <mode>\n", argv[0]);
-	exit(EXIT_FAILURE);
-}
-  
-char prog_mode;
-int fileid,memid;
-char bufer;
-int sem_mutex, sem_full, sem_empty;
-int reader,writer,rexist,wexist;
-
-sem_mutex 	=	sem_get (argv[0], 1);
-sem_full	=	sem_get (argv[0], 2);
-sem_empty	=	sem_get (argv[0], 3);
-reader=sem_get(argv[0],4);
-writer=sem_get(argv[0],5);
-wexist=sem_get(argv[0],6);
-rexist=sem_get(argv[0],7);
-
-
-
-
-
-
-
-
-  
-  if (argc==1) 
-  {
-      prog_mode=READER;
-  } else
-    
-  {
-      prog_mode=WRITER;
-  } 
-  
-  
-  
-  memid=mem_get(argv[0],8);
-
-
-/* --------*/
-/* READER  */  
-/* --------*/
-
-  if (prog_mode==READER)
-  {
-	  
-	  char *mem = (char *)shmat(memid, NULL, 0);
-	  semctl(wexist,0,SETVAL,0);
-	  V(rexist);
-	while(1)
-	{
-		
-		V(reader);
-		int i;
-		for (i=0;i<10;i++)
-		{
-			if (semctl(wexist,0,GETVAL)<=0) sleep(1); else
-			break;
-		}
-		
-		if (semctl(wexist,0,GETVAL)<=0) exit(0);
-		P(writer);
-		if (semctl(wexist,0,GETVAL)<=0) exit(0);
-		P(sem_full);
-		if (semctl(wexist,0,GETVAL)<=0) exit(0);
-		P(sem_mutex);
-		bufer=mem[0];
-		V(sem_mutex);
-		V(sem_empty); 
-		printf("%c",bufer);
-		fflush(stdout);
-		if (semctl(wexist,0,GETVAL)<=0) exit(0);
-		
-	}	
-	return 0;
-  }
-  
-/* --------*/
-/* WRITER  */  
-/* --------*/  
-  
-  if (prog_mode==WRITER)
-  { 
-	  
-	  	int i;
-		for (i=0;i<10;i++)
-		{
-			if (semctl(rexist,0,GETVAL)<=0) sleep(1); else
-			break;
-		}
-		if (semctl(rexist,0,GETVAL)<=0) exit(0);
-	int fileid;
-	fileid = open("in.txt",O_RDONLY,0664);
+		/********/
+		/*GLOBAL*/
+		/********/
 	
-	semctl(sem_mutex,0,SETVAL,1);
-	semctl(sem_empty,0,SETVAL,BUFF_SIZE);
-	semctl(sem_full,0,SETVAL,0);
-	//char str[10]="0123456789";
+	int semid;
+	int shmid;
+	
+	struct sembuf buff[8];
+	void *shm_mem;
+	
+	union semun semunion;
+	
+	unsigned short sems[5]= {0, 0, 0, 0, 0};
+	
+	int i= 3;
+	int count = 1;
+	int fileid;
+	
+	semid = semget(ftok(argv[0], 1), 5, IPC_CREAT | 0664);
+	shmid = shmget(ftok(argv[0], 2), BUFF_SIZE+4, IPC_CREAT | 0664);
+	
+	if (argc == 1)
+	{
+		/********/
+		/*READER*/
+		/********/
 
-	char *mem = (char *)shmat(memid, NULL, 0);
-	semctl(wexist,0,SETVAL,0);
-	struct sembuf mybuf;
-	char str[BUFF_SIZE];
-	V(wexist);
-	int buff_size;
-	while (buff_size=read(fileid,&str,BUFF_SIZE)>0)
-	{
-	for (i=0; i<buff_size; i++)
-	{
-		V(writer);
-		bufer=str[i];
-		if (semctl(rexist,0,GETVAL)<=0) exit(0);
-		P(sem_empty);
-		if (semctl(rexist,0,GETVAL)<=0) exit(0);
-		P(sem_mutex);
-		mem[0]=bufer;
-		V(sem_mutex);
-		V(sem_full); 
-		if (semctl(rexist,0,GETVAL)<=0) exit(0);
-		P(reader);
+
+		addtobuff(&buff[0], SEM_READER, 0, IPC_NOWAIT);
+		addtobuff(&buff[1], SEM_SYNC,   0, IPC_NOWAIT);
+		addtobuff(&buff[2], SEM_READER, +1, SEM_UNDO);
+		semop(semid, buff, 3);
+		if (errno == EAGAIN)
+		{
+			fprintf(stderr, "\n Reader already exists\n");
+			exit(EXIT_FAILURE);
+		}
+		
+		addtobuff(&buff[0], SEM_WRITER, -1, 0);
+		addtobuff(&buff[1], SEM_WRITER, +1, 0);
+		addtobuff(&buff[2], SEM_SYNC,   +1, SEM_UNDO);
+		addtobuff(&buff[3], SEM_FULL,   +1, SEM_UNDO);
+		semop(semid, buff, 4);
+
+
+		addtobuff(&buff[0], SEM_WRITER, -1, IPC_NOWAIT);
+		addtobuff(&buff[1], SEM_WRITER, +1, 0);
+		addtobuff(&buff[2], SEM_SYNC,   -2, 0);
+		addtobuff(&buff[3], SEM_SYNC,   +2, 0);
+		semop(semid, buff, 4);
+		if (errno == EAGAIN)
+		{
+			fprintf(stderr, "\n Sync error\n");
+			exit(EXIT_FAILURE);
+		}
+
+		shm_mem= shmat(shmid, NULL, 0);
+		
+		while (1)
+		{
+			addtobuff(&buff[0], SEM_WRITER, -1, IPC_NOWAIT);
+			addtobuff(&buff[1], SEM_WRITER, +1, 0);
+			addtobuff(&buff[2], SEM_EMPTY,   0, 0);
+			addtobuff(&buff[3], SEM_EMPTY,  +1, 0);
+			semop(semid, buff, 4);
+			
+			if (semctl(semid, SEM_SYNC, GETVAL) == 1)
+			{
+				fprintf(stderr, "\n No writer\n");
+				semunion.array = sems;
+				semctl(semid, SEM_READER, SETALL, semunion);
+				exit(EXIT_FAILURE);
+			}
+
+			count = *((int*)shm_mem);
+			if (count == 0) break;
+			count = write(1, shm_mem+4, count);
+
+
+			addtobuff(&buff[0], SEM_FULL, -1, 0);
+			semop(semid, buff, 1);
+		}
+		
+		addtobuff(&buff[0], SEM_FULL, -1, 0);
+		semop(semid, buff, 1);
+		
+		addtobuff(&buff[0], SEM_SYNC, 0, 0);
+		semop(semid, buff, 1);
 	}
+
+	else if (argc == 2)
+	{
+		/********/
+		/*WRITER*/
+		/********/
+		
+		addtobuff(&buff[0], SEM_WRITER,  0, IPC_NOWAIT);
+		addtobuff(&buff[1], SEM_SYNC,    0, IPC_NOWAIT);
+		addtobuff(&buff[2], SEM_WRITER, +1, SEM_UNDO);
+		semop(semid, buff, 3);
+		
+		if (errno == EAGAIN)
+		{
+			fprintf(stderr, "\n Writer already exists.\n");
+			exit(EXIT_FAILURE);
+		}
+		
+		addtobuff(&buff[0], SEM_READER, -1, 0);
+		addtobuff(&buff[1], SEM_READER, +1, 0);
+		addtobuff(&buff[2], SEM_SYNC,   +1, SEM_UNDO);
+		addtobuff(&buff[3], SEM_EMPTY,  +1, SEM_UNDO);
+		semop(semid, buff, 4);
+
+		addtobuff(&buff[0], SEM_READER, -1, IPC_NOWAIT);
+		addtobuff(&buff[1], SEM_READER, +1, 0);
+		addtobuff(&buff[2], SEM_SYNC,   -2, 0);
+		addtobuff(&buff[3], SEM_SYNC,   +2, 0);
+		semop(semid, buff, 4);
+		
+		if (errno == EAGAIN)
+		{
+			fprintf(stderr, "\n Sync error\n");
+			exit(EXIT_FAILURE);
+		}
+
+		shm_mem= shmat(shmid, NULL, 0);
+		fileid= open(argv[1], O_RDONLY);
+		
+		while (count)
+		{
+
+			count= read(fileid, shm_mem+4, BUFF_SIZE);
+			*((int*)shm_mem)= count;
+
+			addtobuff(&buff[0], SEM_EMPTY,  -1, 0);
+			semop(semid, buff, 1);
+
+			addtobuff(&buff[0], SEM_READER, -1, IPC_NOWAIT);
+			addtobuff(&buff[1], SEM_READER, +1, 0);
+			addtobuff(&buff[2], SEM_FULL,    0, 0);
+			addtobuff(&buff[3], SEM_FULL,   +1, 0);
+			semop(semid, buff, 4);
+			
+			if (semctl(semid, 4, GETVAL) == 1)
+			{
+				fprintf(stderr, "\n No reader\n");
+				semunion.array= sems;
+				semctl(semid, 0, SETALL, semunion);
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		semunion.array= sems;
+		semctl(semid, 0, SETALL, semunion);
 	}
+
+	shmdt(shm_mem);
+
 	return 0;
-  }
-
-  
-
 }
 
